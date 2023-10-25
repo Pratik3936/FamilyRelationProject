c6816897d44806b0001a72a455323ddf2db2bf09
Pratik3936 <pmane03936@gmail.com>
2023-10-25 15:24:34 +0530

    Dao class Commit

---
+++ src/main/java/com/cg/dao/FamilyDao.java
@@ -0,0 +1,15 @@
+package com.cg.dao;
+
+import java.util.List;
+
+import com.cg.entity.Family;
+
+public interface FamilyDao {
+	
+	Family saveFamily(Family family);
+	Family getFamilyById(int id);
+	List<Family>getAllFamilies();
+	Family updateFamily(Family family);
+	Family deleteFamily(Family family);
+
+}
